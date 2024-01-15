650129cd4b7ddf3b9d0e3a62959251c853ef44ef
Allan <allangomes826@gmail.com>
2024-01-15 17:06:46 -0300

    fix ssl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://api.weatherapi.com/v1',
+    baseURL: 'https://api.weatherapi.com/v1',
     params: {
         'key': 'ed5cb95f8ee0401e800220140241201' 
     }
