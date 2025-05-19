1247fe864b28e02b166e80540d842d7057d7a14f
João Victor <jvlerner@uolinc.com>
2025-05-19 01:17:59 -0300

    feat(theme): add theme provider and edit background colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { useMemo } from 'react'
 import { ThemeProvider } from '@emotion/react';
-import { theme } from './services/theme'
+import { lightTheme, darkTheme } from './services/theme';
 import { Dashboard } from './components/Dashboard';
-import { Box } from '@mui/material'
+import { Box, CssBaseline } from '@mui/material'
 import { LocationProvider } from './contexts/LocationContext'
 
 function App() {
+    const theme = useMemo(() => {
+        const hour = new Date().getHours()
+        const isDay = hour >= 6 && hour < 18
+        return isDay ? lightTheme : darkTheme
+    }, [])
+
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <LocationProvider>
                 <Box className='container'>
                     <Dashboard />
